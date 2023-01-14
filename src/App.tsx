0702891210d865ba38017a4c2255f0eca9203301
Todd Zusman <toddzusman@gmail.com>
2023-01-13 19:27:34 -0700

    query loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// @ts-ignore
+import { widgetQuery } from './widget?query'
+// @ts-ignore
+import { widgetQuery as prettyWidgetQuery } from './widget?prettyQuery'
 
+function App() {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Expected:</h2>
+      <pre>
+{`widget {
+  id
+  name
+  color
+  company {
+    id
+    name
+  }
+}`}
+      </pre>
+      <pre>widget {'{'} id name color company {'{'} id name {'}'} {'}'}</pre>
+      <h2>Actual:</h2>
+      <pre>
+        {widgetQuery}
+      </pre>
+      <pre>
+        {prettyWidgetQuery}
+      </pre>
     </div>
   )
 }
